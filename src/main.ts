a751dfb8006709d27898a2c7bcbe2b0e8051db33
artemkolodko <temakolodko@gmail.com>
2023-08-26 11:14:04 +0100

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const logger = new Logger('AppBootstrap');
+
+  const config = new DocumentBuilder()
+    .setTitle('harmony-webcrawler')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = configService.get('port');
   await app.listen(port);
